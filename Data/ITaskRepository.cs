54073b121be36cf2aa2eb806f9a13b7754d79406
Sandeep Cheema <sandeepcheema875@hotmail.com>
2025-12-07 02:29:13 +0600

    Created TaskRepo interface

---
+++ Data/ITaskRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using TaskPro.Model;
+
+namespace TaskPro.Data
+{
+    public interface ITaskRepository
+    {
+        List<TaskItem> load();
+        void Save(List<TaskItem> tasks);
+    }
+
+}
+	
