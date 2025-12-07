6e88c85a1d02ff7d7eabcb003a3e26476e4e7ded
Sandeep Cheema <sandeepcheema875@hotmail.com>
2025-12-07 17:02:14 +0600

    Created a logger to log the tasks using singleton pattern

---
+++ Data/Logger.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskPro.Data
+{
+    public class Logger
+    {
+        private static Logger _instance;
+        private static readonly object _lock = new object();
+        private readonly string _logPath = "activity_log.txt";
+
+        private Logger() { }
+
+        public static Logger GetInstance()
+        {
+            if (_instance == null)
+            {
+                lock (_lock)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new Logger();
+                    }
+                }
+            }
+            return _instance;
+        }
+
+        public void Log(string message)
+        {
+            try
+            {
+                string logEntry = $"{DateTime.Now}: {message}{Environment.NewLine}";
+                File.AppendAllText(_logPath, logEntry);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Failed to write to log: {ex.Message}");
+            }
+        }
+
+    }
+}
